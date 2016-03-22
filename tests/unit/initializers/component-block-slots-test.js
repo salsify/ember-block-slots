e8101f6d7f57a39c28a60c3f0a42672f0495c03d
Michael Carroll <juwara0@users.noreply.github.com>
2016-03-22 09:05:06 -0500

    cleaned up a few lint errors and added test coverage for initializer

--- tests/unit/initializers/component-block-slots-test.js
+++ tests/unit/initializers/component-block-slots-test.js
@@ -9,7 +9,7 @@ import Ember from 'ember'
 import { initialize } from 'ember-block-slots/initializers/component-block-slots'
 
 describe('ComponentBlockSlotsInitializer', function () {
-  let container, application, component
+  let container, application
 
   beforeEach(function () {
     Ember.run(function () {
@@ -26,4 +26,30 @@ describe('ComponentBlockSlotsInitializer', function () {
     // you would normally confirm the results of the initializer here
     expect(true).to.be.ok
   })
-});
+
+  it('"_slots" property is added to the base Component object', function () {
+    initialize(container, application)
+
+    const newComponent = Ember.Component.create()
+
+    expect(newComponent._slots).to.be.an('object')
+  })
+
+  it('"_registerSlot()" is added to the base Component object', function () {
+    initialize(container, application)
+
+    const newComponent = Ember.Component.create()
+
+    expect(newComponent._registerSlot).to.be.a('function')
+  })
+
+  it('"_registerSlot()" sets "_slots.name" to "true"', function () {
+    initialize(container, application)
+
+    const newComponent = Ember.Component.create()
+
+    newComponent._registerSlot('testName')
+
+    expect(newComponent._slots.testName).to.be.ok
+  })
+})
